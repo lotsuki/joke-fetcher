c253f3a56d551e660af3c5234a648ee37ed82224
lotsuki <otsuki.lindsey@gmail.com>
2019-06-05 14:33:48 -0700

    front end set up, webpack/babel set up

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log('hi')
+    return (
+      <div>HI</div>
+    );
+  }
+};
+
+export default App;
