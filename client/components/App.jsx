4908b9bbf4ebac82fea318eddd8bc567a62659dd
lotsuki <otsuki.lindsey@gmail.com>
2019-06-05 15:26:19 -0700

    get request working for random joke on page load

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,10 +4,27 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      joke: '',
+      message: {}
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://jokes-api.herokuapp.com/api/joke')
+    .then(res => res.json())
+    .then(result => {
+      let joke = result.value.joke;
+      let message = result.value;
+      this.setState({ joke, message })
+     })
+    .catch(err => { console.log('Error at GET request: ', err); });
   }
 
   render() {
-    console.log('hi')
+    console.log(this.state.joke, this.state.message)
     return (
       <div>HI</div>
     );
--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/../public`));
+
+app.get('/', (req, res) => {
+  let joke = req.body;
+    res.send(joke);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
