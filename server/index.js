89b0e3e2e257aafb9c729edbb15f01129126ce62
lotsuki <otsuki.lindsey@gmail.com>
2019-06-05 14:04:28 -0700

    html skeleton set up, serve file to server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.static(`${__dirname}/../public/`));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
